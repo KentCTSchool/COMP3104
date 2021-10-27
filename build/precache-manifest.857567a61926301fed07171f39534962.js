ca82978ee570ced3ba660bc7a535f261eab5c76f
Kent Pedrocha <kentcodetoschool@gmail.com>
2021-10-27 17:39:48 -0400

    files updated

---
+++ build/precache-manifest.857567a61926301fed07171f39534962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07a7cc08b7e2aac05b24",
+    "url": "/KentCTSchool/COMP3104/static/css/main.d3d4fffa.chunk.css"
+  },
+  {
+    "revision": "07a7cc08b7e2aac05b24",
+    "url": "/KentCTSchool/COMP3104/static/js/main.07a7cc08.chunk.js"
+  },
+  {
+    "revision": "46ec09458abb6dfe456b",
+    "url": "/KentCTSchool/COMP3104/static/js/runtime~main.46ec0945.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/KentCTSchool/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "b695ef9d8326f1bbe3c3767be2fff299",
+    "url": "/KentCTSchool/COMP3104/index.html"
+  }
+];
