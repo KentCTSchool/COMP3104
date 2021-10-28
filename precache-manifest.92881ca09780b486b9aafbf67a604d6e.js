7221abaae2d92de1c28cb0b1016422d32e097105
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-28 17:58:04 +0000

    Deploy KentCTSchool/COMP3104 to github.com/KentCTSchool/COMP3104.git:gh-pages

---
+++ precache-manifest.92881ca09780b486b9aafbf67a604d6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4af35535a690921e9bab",
+    "url": "/KentCTSchool/COMP3104/static/css/main.f4199f64.chunk.css"
+  },
+  {
+    "revision": "4af35535a690921e9bab",
+    "url": "/KentCTSchool/COMP3104/static/js/main.4af35535.chunk.js"
+  },
+  {
+    "revision": "46ec09458abb6dfe456b",
+    "url": "/KentCTSchool/COMP3104/static/js/runtime~main.46ec0945.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/KentCTSchool/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "d76d91d332ce38ff0479de88032c353e",
+    "url": "/KentCTSchool/COMP3104/index.html"
+  }
+];
